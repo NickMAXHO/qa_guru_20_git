5cbbf89715677c11dd6bbaadb367f7a11acbd6ac
Nikolay_Lakhno <nick.lakhno@gmail.com>
2023-05-24 00:26:24 +0600

    Valera cool

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,8 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa_guru!!!");
+        String msg = "Hello qa_guru!!!";
+
+        System.out.println(msg);
     }
 }
