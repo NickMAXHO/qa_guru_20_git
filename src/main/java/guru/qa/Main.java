779198eee932f6ee89a58b4adc93c1babaf28e60
Nikolay_Lakhno <nick.lakhno@gmail.com>
2023-05-24 17:33:57 +0600

    Loop done

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
-            String msg = "Hello qa_guru!";
+            String msg = "Hello qa_guru 20th stream!";
             System.out.println(msg);
         }
     }
